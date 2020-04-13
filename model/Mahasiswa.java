2fe9bd9e2ff9aaa8dad01e30aaa0bbb5c9ddb383
Mikas17 <mikafullbuster7@gmail.com>
2020-04-14 04:57:12 +0800

    Memanggil

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Mahasiswa ==============");
+  System.out.println("NPM  : 18630653");
+  System.out.println("Nama : Muhammad Mika Sulaksana");
+  System.out.println("====================================");
+ }
+}
